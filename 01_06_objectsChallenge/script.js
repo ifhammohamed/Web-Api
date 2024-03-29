b8bd9a74b76e19e592f436d9042f763fa17b0bdb
Mohamed MRI <mohamedmri.21@uom.lk>
2024-03-29 15:18:36 +0530

    make alert

---
+++ 01_06_objectsChallenge/script.js
@@ -0,0 +1,18 @@
+var cat = {
+  name: "Honey Badger",
+  age: 3,
+  breed: "mix",
+  meow: function () {
+    alert("Meeeeeowwww!");
+  },
+  owner: { name: "Stephanie", state: "CA" },
+};
+
+//Get the cat's name
+var catName = miyaw;
+
+//Get the owner's name
+var catOwner = mia;
+
+//Show a message
+alert(catOwner + " has a cute cat named " + catName);
